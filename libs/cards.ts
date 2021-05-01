833dd3cf30ef5bbb20959d86308bbd26bd617a6c
Jean-Rene Bouvier <24454054+jr-grenoble@users.noreply.github.com>
2021-05-02 00:35:42 +0200

    Full card header class

--- libs/cards.ts
+++ libs/cards.ts
@@ -27,30 +27,114 @@ namespace Cards {
   imageｰstyleｰvalues[imageｰstyles.circle] = CardService.ImageStyle.CIRCLE;
   imageｰstyleｰvalues[imageｰstyles.square] = CardService.ImageStyle.SQUARE;
 
-  export interface headerｰoptions {
-    subtitle?: string;
-    imageｰurl?: string;
-    imageｰstyle?: imageｰstyles;
-    imageｰtext?: string;
+  /**
+   * Card header optional parameters
+   */
+  // constructor trick: a class is both a type and a value,
+  class headerｰoptionｰinterface {
+    constructor(
+      public subtitle?: string,
+      public imageｰurl?: string,
+      public imageｰstyle?: imageｰstyles,
+      public imageｰaltｰtext?: string
+    ) {}
   }
+  const headerｰoptionｰkeys = Object.keys(new headerｰoptionｰinterface());
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface headerｰoptions extends headerｰoptionｰinterface {}
 
   /**
-   * Constructor for a CardHeader.
-   *
-   * @param title The required header title
-   * @param subtitle?  The optional header subtitle
-   * @returns
+   * Encapsulate a card header
    */
-  export function header(
-    title: string,
-    { subtitle, imageｰurl, imageｰstyle, imageｰtext }: headerｰoptions = {}
-  ): GoogleAppsScript.Card_Service.CardHeader {
-    const cardｰheader = CardService.newCardHeader().setTitle(title);
-    if (subtitle) cardｰheader.setSubtitle(subtitle);
-    if (imageｰurl) cardｰheader.setImageUrl(imageｰurl);
-    if (imageｰtext) cardｰheader.setImageAltText(imageｰtext);
-    if (imageｰstyle) cardｰheader.setImageStyle(imageｰstyleｰvalues[imageｰstyle]);
-    return cardｰheader;
+  export class header {
+    private required = { title: "" };
+    private optional: headerｰoptions = {};
+    public readonly cardｰheader: GoogleAppsScript.Card_Service.CardHeader;
+    /**
+     * @constructor builds a card header
+     * @param title The required header title
+     * @param [subtitle] The optional header subtitle
+     * @param [imageｰurl] The optional image (server or data URL)
+     * @param [imageｰtext] The optional image alternate text
+     * @param [imageｰstyle] The optional image style
+     */
+    constructor(
+      title: string,
+      { subtitle, imageｰurl, imageｰstyle, imageｰaltｰtext }: headerｰoptions = {}
+    ) {
+      this.cardｰheader = CardService.newCardHeader().setTitle(
+        (this.required.title = title)
+      );
+      if (subtitle)
+        this.cardｰheader.setSubtitle((this.optional.subtitle = subtitle));
+      if (imageｰurl)
+        this.cardｰheader.setImageUrl((this.optional.imageｰurl = imageｰurl));
+      if (imageｰaltｰtext)
+        this.cardｰheader.setImageAltText(
+          (this.optional.imageｰaltｰtext = imageｰaltｰtext)
+        );
+      if (imageｰstyle)
+        this.cardｰheader.setImageStyle(
+          imageｰstyleｰvalues[(this.optional.imageｰstyle = imageｰstyle)]
+        );
+    }
+    /**
+     ** Accessors
+     **/
+    /**
+     * Get/set header title
+     * @type {String} The header title
+     */
+    get title(): string {
+      return this.required.title;
+    }
+    set title(title: string) {
+      this.cardｰheader.setTitle((this.required.title = title));
+    }
+    /**
+     * Get/set header subtitle
+     * @type {String} The header subtitle
+     */
+    get subtitle(): string {
+      return this.optional.subtitle || "";
+    }
+    set subtitle(subtitle: string) {
+      this.cardｰheader.setSubtitle((this.optional.subtitle = subtitle));
+    }
+    /**
+     * Get/set header image URL (data or regular)
+     * @type {String} The header image URL
+     */
+    get imageｰurl(): string {
+      return this.optional.imageｰurl || "";
+    }
+    set imageｰurl(imageｰurl: string) {
+      this.cardｰheader.setImageUrl((this.optional.imageｰurl = imageｰurl));
+    }
+    /**
+     * Get/set header image alternate text
+     * @type {String} The header image alternate text
+     */
+    get imageｰaltｰtext(): string {
+      return this.optional.imageｰaltｰtext || "";
+    }
+    set imageｰaltｰtext(imageｰaltｰtext: string) {
+      this.cardｰheader.setImageAltText(
+        (this.optional.imageｰaltｰtext = imageｰaltｰtext)
+      );
+    }
+    /**
+     * Get/set header image alternate text
+     * @type {String} The header image alternate text
+     */
+    get imageｰstyle(): imageｰstyles {
+      return this.optional.imageｰstyle || imageｰstyles.square;
+    }
+    set imageｰstyle(imageｰstyle: imageｰstyles) {
+      this.cardｰheader.setImageAltText(
+        imageｰstyleｰvalues[(this.optional.imageｰstyle = imageｰstyle)]
+      );
+    }
   }
 
   // cardｰheader(" title ", { imageｰstyle: imageｰstyle.square });
